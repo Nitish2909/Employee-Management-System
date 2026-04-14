30d33ef330145dca1c3b736969f2847a7f8b65a0
Nitish2909 <nitishraj7043@gmail.com>
2026-04-14 21:12:48 +0530

    #

--- Frontend/src/components/TaskList/TaskList.jsx
+++ Frontend/src/components/TaskList/TaskList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const TaskList = () => {
-  return <div className="bg-amber-400 h-[50%] w-full mt-10">
-    <div className=""></div>
+  return <div className="bg-red-500 overflow-x-auto h-55 m-12 w-[92%] mt-10 py-5 flex items-center justify-start flex-wrap gap-2">
+    <div className="flex-shrink-0 h-full bg-yellow-300  w-[280px] ml-4 rounded-xl">App</div>
+   <div className="flex-shrink-0 h-full bg-yellow-300  w-[280px] ml-4 rounded-xl">App</div>
+   <div className="flex-shrink-0 h-full bg-yellow-300  w-[280px] ml-4 rounded-xl">App</div>
+   <div className="flex-shrink-0 h-full bg-yellow-300  w-[280px] ml-4 rounded-xl">App</div>
+  
+   
   </div>;
 };
 
